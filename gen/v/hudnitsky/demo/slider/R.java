1e4b2781f9196a1730ae00b336d62fb0ebb2acde
Vladimir Hudnitsky <vladimir.hudnitsky@gmail.com>
2013-09-30 07:32:57 +0300

    remove

--- gen/v/hudnitsky/demo/slider/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package v.hudnitsky.demo.slider;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
